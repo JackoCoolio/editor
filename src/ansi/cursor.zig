bebd4afc06f9139d00df00cc82847bfdadf6b8d9
Jackson Wambolt <jtwambolt@gmail.com>
2023-05-12 13:49:51 -0500

    Use std.fmt.bufPrint

--- src/ansi/cursor.zig
+++ src/ansi/cursor.zig
@@ -1,24 +1,14 @@
 const Terminal = @import("../terminal.zig").Terminal;
 const std = @import("std");
 
+/// Move the cursor to the top left.
 pub fn moveCursorHome(term: *const Terminal) !void {
     _ = try term.write("\x1b[H");
 }
 
-fn formatBuf(buf: []u8, comptime fmt: []const u8, args: anytype) !u64 {
-    var stream = std.io.FixedBufferStream([]u8){
-        .buffer = buf,
-        .pos = 0,
-    };
-    var writer = stream.writer();
-
-    try std.fmt.format(writer, fmt, args);
-    const pos = try stream.getPos();
-    return pos;
-}
-
+/// Move the cursor to the given line and column.
 pub fn moveCursor(term: *const Terminal, line: usize, column: usize) !void {
     var buf: [12]u8 = undefined;
-    const len = try formatBuf(&buf, "\x1b[{d};{d}H", .{ line, column });
-    _ = try term.write(buf[0..len]);
+    const slice = try std.fmt.bufPrint(&buf, "\x1b[{d};{d}H", .{ line, column });
+    _ = try term.write(slice);
 }
