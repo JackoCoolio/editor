7f438e59aaca2332942aa34a7a2e9b740004467a
Jackson Wambolt <jtwambolt@gmail.com>
2024-07-09 04:51:06 +0000

    fix build.zig

--- build.zig
+++ build.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const Build = std.Build;
+const Module = Build.Module;
 
 /// Returns a terminfo directory, or null.
 fn getTerminfoDir(allocator: std.mem.Allocator, cmd: []const u8) ?[]u8 {
@@ -73,86 +75,50 @@ fn file_exists(path: []const u8) bool {
 // declaratively construct a build graph that will be executed by an external
 // runner.
 pub fn build(b: *std.Build) !void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
     const terminfo_main_path = "lib/terminfo/src/main.zig";
     std.debug.assert(file_exists(terminfo_main_path));
-    const terminfo_module = b.createModule(std.build.CreateModuleOptions{
-        .source_file = std.build.FileSource{
-            .path = terminfo_main_path,
-        },
+    const terminfo_module = b.createModule(Module.CreateOptions{
+        .root_source_file = b.path(terminfo_main_path),
     });
 
     const gen = b.addSystemCommand(&[_][]const u8{ "python3", "data/gen.py", "data/", unicode_generated_file });
-    const unicode_data_module = b.createModule(std.build.CreateModuleOptions{
-        .source_file = std.build.FileSource{
-            .path = unicode_generated_file,
-        },
+    const unicode_data_module = b.createModule(Module.CreateOptions{
+        .root_source_file = b.path(unicode_generated_file),
     });
 
     const exe = b.addExecutable(.{
         .name = "editor",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    {
-        exe.addModule("terminfo", terminfo_module);
-        exe.addModule("unicode", unicode_data_module);
-    }
-
+    exe.root_module.addImport("terminfo", terminfo_module);
+    exe.root_module.addImport("unicode", unicode_data_module);
     exe.step.dependOn(&gen.step);
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
     b.installArtifact(exe);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
     const run_cmd = b.addRunArtifact(exe);
 
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
     const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    {
-        unit_tests.addModule("terminfo", terminfo_module);
-        unit_tests.addModule("unicode", unicode_data_module);
-    }
+    unit_tests.root_module.addImport("terminfo", terminfo_module);
+    unit_tests.root_module.addImport("unicode", unicode_data_module);
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
 
