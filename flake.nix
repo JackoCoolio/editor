1d5f6977bf0c3578faeeb1055677bb5052653f09
Jackson Wambolt <jtwambolt@gmail.com>
2024-07-09 03:35:16 +0000

    update flake

--- flake.nix
+++ flake.nix
@@ -4,46 +4,54 @@
   inputs = {
     zig.url = "github:mitchellh/zig-overlay";
     zls.url = "github:zigtools/zls";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    zig,
-    zls
-  } @ inputs: let
-    systems = builtins.attrNames inputs.zig.packages;
-  in
-    flake-utils.lib.eachSystem systems (system:
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }:
       let
-        zig-bin = zig.packages.${system}.master;
-        pkgs = import nixpkgs { inherit system; };
+        zig-bin = inputs'.zig.packages.master;
+        zls-bin = inputs'.zls.packages.default;
         deps = {
           build =
             [ zig-bin ] ++
             (with pkgs.python311Packages; [ python requests ]);
-          dev = deps.build ++ [ zls.packages.${system}.default pkgs.gdb ];
+          dev = deps.build ++ [ zls-bin pkgs.gdb ];
         };
       in {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
         packages.editor = pkgs.stdenvNoCC.mkDerivation {
           name = "editor";
-          src = self;
+          src = self';
           dontConfigure = true;
           buildPhase = ''
             CACHE_DIR=$(mktemp -d)
             mkdir -p $out
-            zig build install --global-cache-dir $CACHE_DIR -p $out
-            rm -rf $CACHE_DIR
+            zig build install --global-cache-dir "$CACHE_DIR" -p $out
+            rm -rf "$CACHE_DIR"
           '';
           dontInstall = true;
           buildInputs = deps.build;
         };
 
-        packages.default = self.packages.${system}.editor;
+        packages.default = self'.packages.editor;
 
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = deps.dev;
         };
-      });
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
 }
